10078696360fa0bab85019747caa2285e3ab90a7
unknown <textile.zero@gmail.com>
2022-11-26 12:14:01 +0600

    seller based products return api , jwt token verification on requrest , advertise api to update product advertise status with jwt verification , product delete api implented

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const port = process.env.PORT || 5000;
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { query } = require("express");
 
 app.use(cors())
 app.use(express.json());
@@ -122,6 +123,36 @@ async function run() {
             const result = await productsCollection.insertOne(query);
             res.send(result);
         })
+        // getting my products
+        app.get("/products/:email", verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const query = { sellerEmail: email }
+            const result = await productsCollection.find(query).toArray();
+            res.send(result);
+        })
+        app.get("/products", async (req, res) => {
+            const query = {}
+            const result = await productsCollection.find(query).toArray();
+            res.send(result)
+        })
+        app.delete("/products/:id", verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productsCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // advertise 
+        app.get("/advertise/:id", verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const updateDoc = {
+                $set: { advertise: true }
+            }
+            const result = await productsCollection.updateOne(filter, updateDoc, { upsert: true })
+            res.send(result);
+        })
+
 
     } finally {
 
